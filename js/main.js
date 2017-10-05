926c48f31421ff4e603b4742da7c46bdfec4639c
Yosuke Higuchi <wasapon.dev@gmail.com>
2017-10-05 15:41:10 +0900

    update library

--- js/main.js
+++ js/main.js
@@ -1,19 +1,8 @@
 (function () {
     $(document).ready(function() {
-        //$('g-cube').gcube();
-        //$('g-cube').gspeed(10);
-        //$("g-cube").gscramble("R R");
 
-        $("#btn_scramble").on('click', function() {
-            let str = scrambleGenerator();
+        assignTasks();
 
-            $("g-cube").gscramble(str);
-            $("#lbl_scramble").text("scramble: " + str);
-        });
-
-        $("#btn_reset").on('click', function() {
-            location.reload();
-        });
     });
 
     function scrambleGenerator() {
@@ -31,6 +20,60 @@
         }
 
         return ret;
+    }
+
+    function assignTasks() {
+        $("#btn_scramble").on('click', function() {
+            let str = scrambleGenerator();
+
+            $("g-cube").gscramble(str);
+            $("#lbl_scramble").text("scramble: " + str);
+        });
+
+        $("#btn_reset").on('click', function() {
+            location.reload();
+        });
+
+        $(document).on("keydown", function (e) {
+            let inv = 0;
+
+            if (event.shiftKey) {
+                inv = 6;
+            }
+
+            switch(e.keyCode) {
+                //R
+                case 82:
+                    $("g-cube").gtwist(command[0 + inv]);
+                    break;
+                //L
+                case 76:
+                    $("g-cube").gtwist(command[1 + inv]);
+                    break;
+
+                //U
+                case 85:
+                    $("g-cube").gtwist(command[2 + inv]);
+                    break;
+
+                //D
+                case 68:
+                    $("g-cube").gtwist(command[3 + inv]);
+                    break;
+
+                //F
+                case 70:
+                    $("g-cube").gtwist(command[4 + inv]);
+                    break;
+
+                //B
+                case 66:
+                    $("g-cube").gtwist(command[5 + inv]);
+                    break;
+            }
+
+            return;
+        });
     }
 }());
 
